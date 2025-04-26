0e33c6ca3ca8b1ca1997fdd5f6a7d28ba0c9764c
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2025-04-26 15:41:56 +0530

    Create TestAddition.java

---
+++ TestAddition.java
@@ -0,0 +1,23 @@
+package addition;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import java.util.Random;
+
+public class TestAddition{
+
+    @Test
+    public void testAddWithRandomNumbers() {
+        Random rand = new Random();
+
+        int a = rand.nextInt(100); // random number between 0-99
+        int b = rand.nextInt(100);
+
+        Adder adder = new Adder();
+        int actualResult = adder.add(a, b);
+        int expectedResult = Math.addExact(a, b); // Java built-in addition
+
+        System.out.println("Testing addition: " + a + " + " + b);
+        assertEquals(expectedResult, actualResult, "Addition did not match!");
+    }
+}
